9baad8f740ddb0bc42ecb77a8cf5c9bdd822bd0f
Mike Turley <mike.turley@alum.cs.umass.edu>
2019-07-01 19:46:25 -0400

    Add typeahead select for providers

--- app/javascript/react/screens/App/Analytics/screens/AnalyticsProviderSelection/AnalyticsProviderSelection.js
+++ app/javascript/react/screens/App/Analytics/screens/AnalyticsProviderSelection/AnalyticsProviderSelection.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TypeAheadSelect, Button } from 'patternfly-react';
 
-const AnalyticsProviderSelection = ({ summaryData }) => (
-  <h1>TODO: selection for {summaryData.providers.length} providers</h1>
+const AnalyticsProviderSelection = ({ summaryData, onCancelClick }) => (
+  <React.Fragment>
+    <p>{__('Select providers for which inventory data will be collected.')}</p>
+    <TypeAheadSelect
+      inputProps={{ id: 'providers-select' }}
+      multiple
+      clearButton
+      options={summaryData.providers}
+      labelKey={'name'}
+      placeholder={__('Select...')}
+      emptyLabel={__('No matches found.')}
+      highlightOnlyResult
+      selectHintOnEnter
+    />
+    <br />
+    <p>
+      <strong>{__('Note:')}</strong>
+      &nbsp;
+      {__('Smart State Analysis is not configured. Inventory results will not include operating system introspection.')}
+      <br />
+      {__('To configure Smart State Analysis ..... ')}
+    </p>
+    <div className="footer-buttons">
+      <Button>{__('Continue')}</Button>
+      <Button onClick={onCancelClick}>{__('Cancel')}</Button>
+    </div>
+  </React.Fragment>
 );
 
 AnalyticsProviderSelection.propTypes = {
   summaryData: PropTypes.shape({
-    providers: PropTypes.shape({
-      id: PropTypes.string,
-      name: PropTypes.string
-    })
-  })
+    providers: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string
+      })
+    )
+  }),
+  onCancelClick: PropTypes.func
 };
 
 export default AnalyticsProviderSelection;
