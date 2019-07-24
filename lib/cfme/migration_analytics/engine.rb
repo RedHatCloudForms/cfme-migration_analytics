c96281656ff121e93674cbb648cedd240e0c9a51
Mike Turley <mike.turley@alum.cs.umass.edu>
2019-07-24 11:13:45 -0400

    Temporarily remove the new Migration Analytics menu entry

--- lib/cfme/migration_analytics/engine.rb
+++ lib/cfme/migration_analytics/engine.rb
@@ -15,11 +15,12 @@ module Cfme
         app.config.assets.paths  << root.join('assets', 'images').to_s
       end
 
-      initializer 'plugin-migration-analytics-menu', {:after => 'plugin-migration-menu'} do
-        Menu::CustomLoader.register(
-          Menu::Item.new('migration_analytics', N_('Migration Analytics'), 'migration_analytics', {:feature => 'migration_analytics', :any => true}, '/migration_analytics', :default, :migration)
-        )
-      end
+      # This plugin's menu entry has been removed temporarily while it is incomplete. When the data collection functionality is working, we will un-comment these lines.
+      # initializer 'plugin-migration-analytics-menu', {:after => 'plugin-migration-menu'} do
+      #   Menu::CustomLoader.register(
+      #     Menu::Item.new('migration_analytics', N_('Migration Analytics'), 'migration_analytics', {:feature => 'migration_analytics', :any => true}, '/migration_analytics', :default, :migration)
+      #   )
+      # end
     end
   end
 end
