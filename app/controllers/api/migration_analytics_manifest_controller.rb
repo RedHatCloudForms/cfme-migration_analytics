9ed71c3aacae97a217e585ef138abbceed0ae5dc
Mike Turley <mike.turley@alum.cs.umass.edu>
2019-08-08 11:54:18 -0400

    Remove /tmp manifest upload feature

--- app/controllers/api/migration_analytics_manifest_controller.rb
+++ app/controllers/api/migration_analytics_manifest_controller.rb
@@ -2,21 +2,10 @@ module Api
   class MigrationAnalyticsManifestController < BaseController
 
     def index
-      tmp_manifest_path = '/tmp/migration-analytics-manifest.json'
-      tmp_manifest = File.file?(tmp_manifest_path) ? load_manifest(tmp_manifest_path) : nil
-      tmp_manifest_valid = tmp_manifest != nil && tmp_manifest != :invalid
-
-      if tmp_manifest_valid
-        manifest_path = tmp_manifest_path
-        manifest = tmp_manifest
-      else
-        manifest_path = Cfme::MigrationAnalytics::Engine.root.join('config', 'default-manifest.json')
-        manifest = load_manifest(manifest_path)
-      end
+      manifest_path = Cfme::MigrationAnalytics::Engine.root.join('config', 'default-manifest.json')
+      manifest = load_manifest(manifest_path)
 
       res = {
-        :tmp_manifest_present => tmp_manifest != nil,
-        :tmp_manifest_valid => tmp_manifest_valid,
         :path => manifest_path,
         :body => manifest,
       }
@@ -28,7 +17,7 @@ module Api
     def load_manifest(path)
         JSON.parse(File.read(path))
       rescue JSON::ParserError
-        :invalid
+        nil
     end
 
   end
