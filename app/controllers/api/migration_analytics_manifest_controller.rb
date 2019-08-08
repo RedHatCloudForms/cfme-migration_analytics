3b1e74f9e81a9812ce0358618b66be8bbcfe1194
Adam Grare <agrare@redhat.com>
2019-08-08 11:57:10 -0400

    Merge pull request #8 from mturley/manifest-api-endpoint
    Add default manifest and API controller for loading it
    
    (cherry picked from commit 3847740a36fa5bb0a4fcad174160d1419165cb7c)

---
+++ app/controllers/api/migration_analytics_manifest_controller.rb
@@ -0,0 +1,24 @@
+module Api
+  class MigrationAnalyticsManifestController < BaseController
+
+    def index
+      manifest_path = Cfme::MigrationAnalytics::Engine.root.join('config', 'default-manifest.json')
+      manifest = load_manifest(manifest_path)
+
+      res = {
+        :path => manifest_path,
+        :body => manifest,
+      }
+      render_resource :migration_analytics_manifest, res
+    end
+
+    private
+
+    def load_manifest(path)
+        JSON.parse(File.read(path))
+      rescue JSON::ParserError
+        nil
+    end
+
+  end
+end
