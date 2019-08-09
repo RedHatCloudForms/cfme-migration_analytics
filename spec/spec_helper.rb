cb13a79c31e3784e00172f2b8ec031217b61dddb
Adam Grare <agrare@redhat.com>
2019-08-09 15:27:10 -0400

    Merge pull request #9 from d-m-u/adding_collection_endpoint
    Collection endpoint start that calls bundle
    
    (cherry picked from commit d7cd623fd356e5582b5ed4dda120477430328f46)

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+if ENV['CI']
+  require 'simplecov'
+  SimpleCov.start
+end
+
+Dir[Rails.root.join("spec/shared/**/*.rb")].each { |f| require f }
+Dir[File.join(__dir__, "support/**/*.rb")].each { |f| require f }
+
+RSpec.configure do |config|
+  config.include Spec::Support::ApiRequestHelpers, :type => :request
+
+  config.before(:each, :type => :request) { init_api }
+end
+
+require "cfme-migration_analytics"
