36e818958130e1bac658856982bd4e2d5a36b4cb
Mike Turley <mike.turley@alum.cs.umass.edu>
2019-08-15 18:02:01 -0400

    Remove the protocol/host/port nonsense, just use a relative path, duh. oops.

--- app/javascript/react/screens/App/Analytics/screens/AnalyticsDataCollection/helpers.js
+++ app/javascript/react/screens/App/Analytics/screens/AnalyticsDataCollection/helpers.js
@@ -2,8 +2,5 @@ const payloadPathRegex = /\/migration_analytics\/.+$/;
 
 export const getPayloadUrl = payloadPath => {
   const match = payloadPath && payloadPath.match(payloadPathRegex);
-  if (!match) return null;
-  const { protocol, hostname, port } = window.location;
-  const origin = `${protocol}//${hostname}${port ? `:${port}` : ''}`;
-  return `${origin}${match[0]}`;
+  return match ? match[0] : null;
 };
