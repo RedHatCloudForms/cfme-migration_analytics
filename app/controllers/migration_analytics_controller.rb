1e8867d9a3c149d4f00c3e05f60fdf67863184a4
Martin Povolny <mpovolny@redhat.com>
2020-01-20 17:54:37 +0100

    Add payload download action to the UI worker.

--- app/controllers/migration_analytics_controller.rb
+++ app/controllers/migration_analytics_controller.rb
@@ -13,5 +13,30 @@ class MigrationAnalyticsController < ApplicationController
     end
   end
 
+  def payload
+    check_feature_enabled
+    task_id = params['task_id']
+    raise "Must specify a task id via \"task_id\"" if task_id.blank?
+
+    begin
+      task = MiqTask.find(task_id)
+      path = task&.context_data&.payload_path
+      if path && File.exist?(path)
+        send_file(path, :type => 'application/binary', :filename => 'analytics_payload.bin')
+      else
+        raise "Payload not found."
+      end
+    rescue ActiveRecord::RecordNotFound => e
+      action_result(false, e.to_s)
+    end
+  end
+
   menu_section :migration
+
+  private
+  def check_feature_enabled
+    unless Settings.prototype.migration_analytics.enabled
+      raise ActionController::RoutingError, 'Feature Not Enabled'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   if Settings.prototype.migration_analytics.enabled
     get "/migration_analytics", to: "migration_analytics#index"
+    get "/migration_analytics/payload", to: "migration_analytics#payload"
   end
 end
