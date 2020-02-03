07270569eb1b2c488116bfdbd3feb5fd7ef6742c
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-02-03 14:36:17 -0500

    Accept additional options in API wrapper

--- app/javascript/common/API.js
+++ app/javascript/common/API.js
@@ -1,32 +1,41 @@
 const { API } = window;
 
+// TODO this is probably unnecessary, it was copied from v2v and originally used axios directly,
+// but was kept around to maintain the call and response signature without changing all the usages.
+// We might want to refactor action creators to instead use window.API directly.
+
 export default {
-  get(url, headers = {}, params = {}) {
+  get(url, headers = {}, params = {}, otherOptions = {}) {
     return API.get(url, {
       transformResponse: e => ({ data: e }),
       headers,
-      params
+      params,
+      ...otherOptions
     });
   },
-  put(url, data = {}, headers = {}) {
+  put(url, data = {}, headers = {}, otherOptions = {}) {
     return API.put(url, data, {
-      headers
+      headers,
+      ...otherOptions
     });
   },
-  post(url, data = {}, headers = {}) {
+  post(url, data = {}, headers = {}, otherOptions = {}) {
     return API.post(url, data, {
       headers,
-      transformResponse: e => ({ data: e })
+      transformResponse: e => ({ data: e }),
+      ...otherOptions
     });
   },
-  delete(url, headers = {}) {
+  delete(url, headers = {}, otherOptions = {}) {
     return API.delete(url, {
-      headers
+      headers,
+      ...otherOptions
     });
   },
-  patch(url, data = {}, headers = {}) {
+  patch(url, data = {}, headers = {}, otherOptions = {}) {
     return API.patch(url, data, {
-      headers
+      headers,
+      ...otherOptions
     });
   }
 };
